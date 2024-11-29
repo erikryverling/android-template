aea222ab3329a765ef788ac088a53ea9d2f6596e
Erik R. Yverling <erik.r@yverling.se>
2024-11-29 16:18:38 +0100

    Bump dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,6 @@ android {
     defaultConfig {
         applicationId = "se.yverling.template"
         minSdk = 26
-        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
     }
